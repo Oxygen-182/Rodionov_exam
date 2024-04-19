b962979aa581eb44e4e2d15fba06e633d5e181fc
stu-pksp120 <stu-pksp120@AMDVLA3511GW10>
2024-04-19 09:37:09 +0300

    Создание классов и заполнение массива

--- scr/Program.cs
+++ scr/Program.cs
@@ -8,8 +8,51 @@ namespace scr
 {
     class Program
     {
+        class Indications
+        {
+            public int temper;
+            public int vlasz;
+            public int davl;
+            public Indications (int temper, int vlasz, int davl)
+            {
+                this.temper = temper;
+                this.vlasz = vlasz;
+                this.davl = davl;
+            }
+        }
+        class WeatherControl
+        {
+            public Indications[] indi;
+            public WeatherControl (Indications[] indi)
+            {
+                this.indi = indi;
+            }
+        }
         static void Main(string[] args)
         {
+            int n;
+            Console.WriteLine("Введите размерность массива");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Indications[] masIndi = new Indications[n];
+            WeatherControl wc = new WeatherControl(masIndi);
+            int temper, vlasz, davl;
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("\nВведите температуру");
+                temper = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите влажность");
+                vlasz = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите давление");
+                davl = Convert.ToInt32(Console.ReadLine());
+                wc.indi[i] = new Indications(temper, vlasz, davl);
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"\nПоказатели номер {i+1}\nТемература = {wc.indi[i].temper}\nВлажномть = {wc.indi[i].vlasz}\nДавление = {wc.indi[i].davl}\n");
+            }
         }
     }
 }
